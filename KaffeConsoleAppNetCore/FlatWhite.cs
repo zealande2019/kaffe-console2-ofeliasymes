5ffaf2680e524b1cb394f87a20c69f9d13083225
ofeliasymes <ofeliasymes@gmail.com>
2019-02-03 20:33:35 +0100

    Tilføjet Flatwhite med 160 ml. mælk, 45 i pris og mild styrke.

---
+++ KaffeConsoleAppNetCore/FlatWhite.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using kaffe;
+
+namespace KaffeConsoleAppNetCore
+{
+    class FlatWhite : Kaffe, Imælk
+    {
+        public int MlMælk()
+        {
+            return 160;
+        }
+
+        public override int Pris()
+        {
+            return 45;
+        }
+
+        public override string Styrke()
+        {
+            return "Mild";
+        }
+
+        public FlatWhite(int rabat) :base(rabat)
+        {
+
+        }
+    }
+}
--- KaffeConsoleAppNetCore/Program.cs
+++ KaffeConsoleAppNetCore/Program.cs
@@ -1,5 +1,6 @@
 ﻿using kaffe;
 using System;
+using System.Collections.Generic;
 
 namespace KaffeConsoleAppNetCore
 {
@@ -8,11 +9,36 @@ namespace KaffeConsoleAppNetCore
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            FlatWhite newFlatwhite = new FlatWhite(10);
+            Console.WriteLine(newFlatwhite.Pris());
+            Console.WriteLine(newFlatwhite.Styrke());
+            Console.WriteLine(newFlatwhite.MlMælk());
+            Console.WriteLine(newFlatwhite.Rabat);
+            Console.ReadKey();
 
+            Cortado cortado = new Cortado();
+            Latte latte = new Latte();
+            FlatWhite flatWhite = new FlatWhite(12);
+
+            List<Imælk> liste = new List<Imælk>();
+            liste.Add(cortado);
+            liste.Add(latte);
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine("Før cast " + item.MlMælk());
+
+                if (item is Latte)
+                {
+                    latte = item as Latte;
+                    Console.WriteLine("Latte styrke" + latte.Styrke());
+                }
+                else if (item is Cortado)
+                {
+                    Cortado c = item as Cortado;
+                }
+            }
             
-
-           
-
         }
     }
 }
